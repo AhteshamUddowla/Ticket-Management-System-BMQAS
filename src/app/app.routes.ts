b4bf00b97d65d40e88044188f3be4f16d8f4d16d
AhteshamUddowla <ahteshamiiuc@gmail.com>
2025-02-26 12:16:12 +0600

    feature()-navbar added , refactor()- gitignore

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { TicketListComponent } from './components/ticket-list/ticket-list.component';
+import { UserListComponent } from './components/user-list/user-list.component';
+import { UserDetailsComponent } from './components/user-details/user-details.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', redirectTo: 'dashboard', pathMatch: 'full'},
+    {path: 'dashboard', component: DashboardComponent, title: 'Dashboard'},
+    {path: 'tickets', component: TicketListComponent, title: 'Tickets'},
+    {path: 'users', component: UserListComponent, title: 'Users'},
+    {path: 'users-details', component: UserDetailsComponent, title: 'User details'},
+];
--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
+import { MaterialModule } from '../../modules/angular-material/angular-material.module';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [MaterialModule, RouterOutlet, RouterLink, RouterLinkActive],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
 export class NavbarComponent {
-
+  isBadgeVisible:boolean = false
+  badgeVisibility(){
+    this.isBadgeVisible = !this.isBadgeVisible
+  }
 }
